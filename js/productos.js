fbb1583027c7e138901098cf67a14e41ff1c3326
Carlos Mallo Ruiz <carlosmalloruiz@gmail.com>
2026-09-11 17:54:17 +0200

    Add files via upload

--- js/productos.js
+++ js/productos.js
@@ -9,7 +9,7 @@
 const PRODUCTOS = [
   {
     id: "rolex-batgirl",
-    nombre: "Rolex GMT-Master II Batgirl",
+    nombre: "Rlx GMT-Master II Batgirl",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexMasterIINegroAzul.png",
@@ -18,7 +18,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-rootbeer",
-    nombre: "Rolex GMT-Master II Oro Rosa",
+    nombre: "Rlx GMT-Master II Oro Rosa",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexOroRosa.png",
@@ -27,7 +27,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-batman",
-    nombre: "Rolex GMT-Master II Batman",
+    nombre: "Rlx GMT-Master II Batman",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexBatman.png",
@@ -36,7 +36,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-yacht-master",
-    nombre: "Rolex Yacht Master Oro Rosa con Correa Oysterflex",
+    nombre: "Rlx Yacht Master Oro Rosa con Correa Oysterflex",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexOroRosaCierreGoma.png",
@@ -45,7 +45,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-bruce-wayne",
-    nombre: "Rolex GMT-Master II Bruce Wayne",
+    nombre: "Rlx GMT-Master II Bruce Wayne",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexMasterIINegroGris.png",
@@ -54,7 +54,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-oro-efera-celeste",
-    nombre: "Rolex Cosmograph Daytona Oro Amarillo con Esfera Azul Celeste",
+    nombre: "Rlx Cosmograph Daytona Oro Amarillo con Esfera Azul Celeste",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexDaytonaDoradoAzulCierreGoma.png",
@@ -63,7 +63,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-daytona-amarillo-dorado",
-    nombre: "Rolex Cosmograph Daytona Oro Amarillo / Dorado",
+    nombre: "Rlx Cosmograph Daytona Oro Amarillo / Dorado",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexDaytonaDoradoCierreGoma.png",
@@ -72,7 +72,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-daytona-plata",
-    nombre: "Rolex Cosmograph Daytona Acero Esfera Plata",
+    nombre: "Rlx Cosmograph Daytona Acero Esfera Plata",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexDaytonaCierreGoma.png",
@@ -81,7 +81,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-dorado-cierre-goma",
-    nombre: "Rolex Yacht Master Oro Amarillo con Correa Oysterflex",
+    nombre: "Rlx Yacht Master Oro Amarillo con Correa Oysterflex",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexDoradoCierreGoma.png",
@@ -90,7 +90,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-yacht-master-esfera-azul",
-    nombre: "Rolex Yacht Master Acero Platino con Esfera Azul",
+    nombre: "Rlx Yacht Master Acero Platino con Esfera Azul",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/Rolex.png",
@@ -99,7 +99,7 @@ const PRODUCTOS = [
   },
   {
     id: "rolex-yacht-master-esfera-azul",
-    nombre: "Rolex Yacht Master 42 en Titanio RLX Oro Blanco con Correa Oysterflex",
+    nombre: "Rlx Yacht Master 42 en Titanio RLX Oro Blanco con Correa Oysterflex",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/RolexYachtMaster42.png",
@@ -108,7 +108,7 @@ const PRODUCTOS = [
   },
   {
     id: "cartier-diamantes-negros",
-    nombre: "Cartier Santos de Cartier Iced Out en Diamantes Negros",
+    nombre: "Crtr Santos de Cartier Iced Out en Diamantes Negros",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/CartierPerladoNegro.png",
@@ -117,7 +117,7 @@ const PRODUCTOS = [
   },
   {
     id: "omega-snoopy",
-    nombre: "Omega x Swatch Bioceramic MoonSwatch Mission to the Moonphase (Snoopy White)",
+    nombre: "Om x Sw Bioceramic MoonSwatch Mission to the Moonphase (Snoopy White)",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/OmegaBlanco.png",
@@ -129,7 +129,7 @@ const PRODUCTOS = [
   },
   {
     id: "richard-mille-rafael-nadal",
-    nombre: "Richard Mille RM 35-02 Rafael Nadal Carbono NTPT con Correa Azul",
+    nombre: "RM 35-02 Rafael Nadal Carbono NTPT con Correa Azul",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/RichardGomaAzul.png",
@@ -138,7 +138,7 @@ const PRODUCTOS = [
   },
   {
     id: "richard-mille-rafael-nadal-blanco",
-    nombre: "Richard Mille RM 35-02 Rafael Nadal Cerámica Blanca con Correa Blanca",
+    nombre: "RM 35-02 Rafael Nadal Cerámica Blanca con Correa Blanca",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/RichardGomaBlanca.png",
@@ -147,7 +147,7 @@ const PRODUCTOS = [
   },
   {
     id: "richard-mille-rafael-nadal-negro",
-    nombre: "Richard Mille RM 35-02 Rafael Nadal Carbono NTPT con Correa Negra",
+    nombre: "RM 35-02 Rafael Nadal Carbono NTPT con Correa Negra",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/RichardGomaNegra.png",
@@ -156,7 +156,7 @@ const PRODUCTOS = [
   },
   {
     id: "richard-mille-rm-53-01-azul",
-    nombre: "Richard Mille RM 53-01 Tourbillon Pablo Mac Donough Zafiro con Correa Azul",
+    nombre: "RM 53-01 Tourbillon Pablo Mac Donough Zafiro con Correa Azul",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/RichardTrasparenteGomaAzul.png",
@@ -165,7 +165,7 @@ const PRODUCTOS = [
   },
   {
     id: "richard-mille-rm-53-01-negro",
-    nombre: "Richard Mille RM 53-01 Tourbillon Pablo Mac Donough Zafiro con Correa Negra",
+    nombre: "RM 53-01 Tourbillon Pablo Mac Donough Zafiro con Correa Negra",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/RichardTrasparenteGomaNegra.png",
@@ -174,7 +174,7 @@ const PRODUCTOS = [
   },
   {
     id: "ap-dorado",
-    nombre: "Audemars Piguet Royal Oak Cronógrafo Oro Amarillo",
+    nombre: "AP Royal Oak Cronógrafo Oro Amarillo",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/ApDorado.png",
@@ -183,7 +183,7 @@ const PRODUCTOS = [
   },
   {
     id: "ap-negro",
-    nombre: "Audemars Piguet Royal Oak Cronógrafo Cerámica Negra",
+    nombre: "AP Royal Oak Cronógrafo Cerámica Negra",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/ApFullBlack.png",
@@ -192,7 +192,7 @@ const PRODUCTOS = [
   },
   {
     id: "ap-oro-rosa",
-    nombre: "Audemars Piguet Royal Oak Cronógrafo Oro Rosa",
+    nombre: "AP Royal Oak Cronógrafo Oro Rosa",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/ApOroRosa.png",
@@ -201,7 +201,7 @@ const PRODUCTOS = [
   },
   {
     id: "ap-plata-celeste",
-    nombre: "Audemars Piguet Royal Oak Cronógrafo Acero con Esfera Azul Celeste",
+    nombre: "AP Royal Oak Cronógrafo Acero con Esfera Azul Celeste",
     categoria: "Relojes",
     precio: 49.99,
     imagen: "assets/productos/ApPlateadoEferaCeleste.png",
@@ -210,7 +210,7 @@ const PRODUCTOS = [
   },
   {
     id: "ap-perlado-blanco",
-    nombre: "Audemars Piguet Royal Oak Iced Out Diamantes Blancos",
+    nombre: "AP Royal Oak Iced Out Diamantes Blancos",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/ApPerladoBlanco.png",
@@ -219,31 +219,67 @@ const PRODUCTOS = [
   },
   {
     id: "ap-perlado-negro",
-    nombre: "Audemars Piguet Royal Oak Iced Out Diamantes Negros",
+    nombre: "AP Royal Oak Iced Out Diamantes Negros",
     categoria: "Relojes",
     precio: 59.99,
     imagen: "assets/productos/ApPerladoNegro.png",
     descripcion: "Reloj de tres agujas con ventana de fecha, cubierto por completo en pavé de diamantes en tono negro brillante, abarcando caja, bisel, esfera y brazalete.",
     tallas: ["Talla única"]
   },
-  /*{
+  {
     id: "cinturon-cuero-clasico",
-    nombre: "Cinturón de Cuero Clásico",
+    nombre: "Pack cinturon y cartera LV Dorado",
     categoria: "Cinturones",
-    precio: 24.90,
-    imagen: "assets/productos/lvcinturon.webp",
-    descripcion: "Cinturón de cuero genuino con hebilla metálica de acabado dorado. Versátil para looks formales e informales.",
-    tallas: ["Talla única"]
+    precio: 44.99,
+    imagen: "assets/productos/lvdorado.jpeg",
+    descripcion: "Conjunto compuesto por cinturón con hebilla en acabado dorado, y billetera a juego. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
   },
   {
     id: "cinturon-reversible-negro-marron",
-    nombre: "Cinturón Reversible Negro/Marrón",
+    nombre: "Pack cinturon y cartera LV Plata",
     categoria: "Cinturones",
-    precio: 27.90,
-    imagen: "https://placehold.co/800x800/141210/c6a15b?text=IMPERIOWATCH",
-    descripcion: "Cinturón reversible con hebilla giratoria: dos colores en una sola pieza. Práctico y elegante para cualquier ocasión.",
-    tallas: ["Talla única"]
-  },*/
+    precio: 44.99,
+    imagen: "assets/productos/lvplata.jpeg",
+    descripcion: "Conjunto compuesto por cinturón en lona Monogram con hebilla en acabado plateado, y billetera a juego. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
+  },
+  {
+    id: "cinturon-cuero-clasico",
+    nombre: "Pack cinturon y cartera Hrms Dorado",
+    categoria: "Cinturones",
+    precio: 44.99,
+    imagen: "assets/productos/hermesdorado.jpeg",
+    descripcion: "Conjunto compuesto por cinturón de piel negra con hebilla en acabado dorado, y billetera a juego con detalle metálico. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
+  },
+  {
+    id: "cinturon-reversible-negro-marron",
+    nombre: "Pack cinturon y cartera Hrms Plata",
+    categoria: "Cinturones",
+    precio: 44.99,
+    imagen: "assets/productos/hermesplata.jpeg",
+    descripcion: "Conjunto compuesto por cinturón de piel negra con hebilla en acabado plateado, y billetera a juego con detalle metálico. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
+  },
+  {
+    id: "cinturon-cuero-clasico",
+    nombre: "Pack cinturon y cartera GC Negro",
+    categoria: "Cinturones",
+    precio: 44.99,
+    imagen: "assets/productos/guccinegro.jpeg",
+    descripcion: "Conjunto compuesto por cinturón en tono gris con hebilla en acabado negro mate, y billetera a juego. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
+  },
+  {
+    id: "cinturon-reversible-negro-marron",
+    nombre: "Pack cinturon y cartera GC Verde y Negro",
+    categoria: "Cinturones",
+    precio: 44.99,
+    imagen: "assets/productos/gucciverdenegro.jpeg",
+    descripcion: "Conjunto compuesto por cinturón en piel negra con detalle tricolor en la hebilla, y billetera a juego con franja verde y roja. Incluye bolsa de compra y caja original de la marca.",
+    tallas: ["110 cm"]
+  },
 ];
 
 function getProducto(id){
